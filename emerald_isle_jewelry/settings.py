f54382ed448e2ad4caa1cdc5759f1eab9319462a
oconnorian3 <oconnorian3@gmail.com>
2023-07-12 19:34:34 +0000

    Add index.html and home app

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,10 @@ ROOT_URLCONF = 'emerald_isle_jewelry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
