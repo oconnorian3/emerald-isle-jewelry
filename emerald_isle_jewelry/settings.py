4d3676b77ae1d88127ca074745332753c85fe346
oconnorian3 <oconnorian3@gmail.com>
2023-07-20 20:09:18 +0000

    change env.py variables

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from env import DATABASE_URL
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
