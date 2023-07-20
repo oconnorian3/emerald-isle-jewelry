343964e011104afa07f3fa70c7c50298b1964dd8
oconnorian3 <oconnorian3@gmail.com>
2023-07-20 21:37:50 +0000

    Update requirments.txt

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# 1SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*v4cbbi9tp_j890e1vvbp^sctxk5)9h#tw4)eq*uroxmetowem'
 
 # SECURITY WARNING: don't run with debug turned on in production!
