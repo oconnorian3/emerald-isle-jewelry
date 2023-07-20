9a3f0ad2d50c1a94c81ee09f8953de6328ac3404
oconnorian3 <oconnorian3@gmail.com>
2023-07-20 20:05:18 +0000

    change env.py variables

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-
-if os.path.isfile("env.py"):
-    import env
-
+from env import DATABASE_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,17 +107,20 @@ WSGI_APPLICATION = 'emerald_isle_jewelry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DATABASE_URL,
     }
-#else:
-    #DATABASES = {
-        #'default': {
-            #'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': BASE_DIR / 'db.sqlite3',
-        #}
-    #}
+}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
