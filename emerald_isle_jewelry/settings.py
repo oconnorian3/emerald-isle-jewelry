71e3aa3ee7b5e2c4f79714de637f28cb6cbaf2e5
oconnorian3 <oconnorian3@gmail.com>
2023-07-20 21:24:11 +0000

    chjange more db variables for T/s

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -111,18 +111,15 @@ WSGI_APPLICATION = 'emerald_isle_jewelry.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default':
-            'Engine': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get('DATABASE_URL'),
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
