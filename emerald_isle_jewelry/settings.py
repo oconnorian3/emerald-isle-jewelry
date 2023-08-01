775b41ece9ca1ca379bc41d981346901e420286a
oconnorian3 <oconnorian3@gmail.com>
2023-08-01 09:50:22 +0000

    Add crispy forms and checkout.html page

--- emerald_isle_jewelry/settings.py
+++ emerald_isle_jewelry/settings.py
@@ -59,6 +59,9 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +77,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'emerald_isle_jewelry.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -88,8 +93,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
