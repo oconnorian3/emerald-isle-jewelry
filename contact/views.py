6c783365a61d199b2abb11f1caac1bbce8c2a962
oconnorian3 <oconnorian3@gmail.com>
2023-08-05 21:54:59 +0000

    change view so submissions save

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.core.mail import send_mail
 from django.conf import settings
 from .models import ContactFormSubmission
 
 def render_contact_form(request):
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            
+            return redirect('home')  # Replace 'success-page' with the URL of a success page
+    else:
+        form = ContactForm()
+
     return render(request, 'contact/contact_form.html', {'form': form})
