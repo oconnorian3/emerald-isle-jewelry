5e3d93c122bd6a222ec4ca4ef6a9c321b07779c0
oconnorian3 <oconnorian3@gmail.com>
2023-08-07 18:45:07 +0000

    Added sign up form

--- contact/views.py
+++ contact/views.py
@@ -4,17 +4,19 @@ from django.core.mail import send_mail
 from django.conf import settings
 from .models import ContactFormSubmission
 
+
 def render_contact_form(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()  # Save the form data to the database
+            form.save()
             
-            return redirect('contact_success')  # Replace 'success-page' with the URL of a success page
+            return redirect('contact_success')
     else:
         form = ContactForm()
 
     return render(request, 'contact/contact_form.html', {'form': form})
+
 
 def contact_success(request):
     return render(request, 'contact/contact_success.html')
