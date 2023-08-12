3d955259c44a947b0178069c5a5f495396e7b405
oconnorian3 <oconnorian3@gmail.com>
2023-08-12 15:25:57 +0000

    Add product field to testimonials

--- testomonial/models.py
+++ testomonial/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Testimonial(models.Model):
     author = models.CharField(max_length=100)
+    product = models.CharField(max_length=100, default='Unknown Product')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
