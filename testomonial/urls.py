779770d6949c5d6eb40161f0fb1c7021d47162d7
oconnorian3 <oconnorian3@gmail.com>
2023-08-12 14:58:05 +0000

    Fixed testimonial list page

--- testomonial/urls.py
+++ testomonial/urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.add_testimonial, name='add_testimonial'),
-    path('testimonial_list/', views.add_testimonial, name='add_testimonial'),
+    path('list/', views.testimonial_list, name='testimonial_list'),
 ]
--- testomonial/views.py
+++ testomonial/views.py
@@ -9,7 +9,12 @@ def add_testimonial(request):
         form = TestimonialForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('testomonial/testimonial_list') 
+            return redirect('testimonial_list') 
     else:
         form = TestimonialForm()
     return render(request, 'testomonial/add_testimonial.html', {'form': form})
+
+
+def testimonial_list(request):
+    testimonials = Testimonial.objects.all()  # Retrieve all testimonials from the database
+    return render(request, 'testomonial/testimonial_list.html', {'testimonials': testimonials})
