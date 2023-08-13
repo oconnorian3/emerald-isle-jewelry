ee05821e8d62636ec26c0a9e054f6f53b2012cf8
oconnorian3 <oconnorian3@gmail.com>
2023-08-13 11:47:00 +0000

    Add link to saved for later product

--- saved/models.py
+++ saved/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from products.models import Product
 
 
@@ -7,3 +8,7 @@ class SavedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
+
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[str(self.slug)])
