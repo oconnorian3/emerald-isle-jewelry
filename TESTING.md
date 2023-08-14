df4ed83a3226da5401eea06c6cdec5506593199a
oconnorian3 <oconnorian3@gmail.com>
2023-08-14 19:32:48 +0000

    Cleaned up checkout app code

---
+++ TESTING.md
@@ -0,0 +1,11 @@
+## Testing (Post Development)
+
+**Validation**
+
+[HTML](https://validator.w3.org/nu/?doc=https%3A%2F%2Femerald-isle-jewelry-af11dcd57db0.herokuapp.com)
+
+All pages passed the above validator except of the contact page. Confirmed it does indeed work but unable to address the below as this would require a complete refactor which is not possible due to time contraints. 
+
+![](media/validator.png)
+
+[CSS](https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Femerald-isle-jewelry-af11dcd57db0.herokuapp.com&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en)
--- checkout/views.py
+++ checkout/views.py
@@ -1,32 +1,34 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+import stripe
+import json
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from profiles.forms import UserProfileForm
 from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
-import stripe
-import json
 
 @require_POST
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
+        metadata = {
             'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
-        })
+        }
+        stripe.PaymentIntent.modify(pid, metadata=metadata)
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(request, (
+            'Sorry, your payment cannot be processed right now. '
+            'Please try again later.'
+        ))
         return HttpResponse(content=e, status=400)
 
 
@@ -76,21 +78,23 @@ def checkout(request):
                             order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
-                        "Please call us for assistance!")
-                    )
+                        "One of the products wasn't found in our database. "
+                        "Please call us for assistance!"
+                    ))
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
+            messages.error(request, (
+                'There was an error with your form. '
+                'Please double-check your information.'
+            ))
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request, "Theres nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
@@ -122,8 +126,9 @@ def checkout(request):
             order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        messages.warning(request, (
+            'Stripe public key is missing.orget to set it in your environment?'
+        ))
 
     template = 'checkout/checkout.html'
     context = {
@@ -163,9 +168,10 @@ def checkout_success(request, order_number):
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+    messages.success(request, (
+        f'Order successfully processed! Your order number is {order_number}. '
+        f'A confirmation email will be sent to {order.email}.'
+    ))
 
     if 'bag' in request.session:
         del request.session['bag']
