4ff45b566f2d920bf472bae409ff6d6ff84aeb0a
oconnorian3 <oconnorian3@gmail.com>
2023-09-21 18:51:48 +0000

    Define HttpResponse in checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.contrib import messages
